b28e47f6e8f3866211e5909208c79ed0173d893f
Rohan <83433649+RohanSen08@users.noreply.github.com>
2026-01-07 02:48:15 -0500

    moved shared mem

---
+++ shared.c
@@ -0,0 +1,38 @@
+#include <signal.h>
+
+void create(){
+  //Shared memory
+  int shmd;
+  int *data;
+  shmd = shmget(key, sizeof(int), IPC_CREAT | 0640);
+  if(shmd < 0)err();
+  data = shmat(shmd,0,0);
+  if(data == (void*)-1)err();
+  shmdt(data);
+  //Semaphore
+  int semd = semget(key, 1, IPC_CREAT | IPC_EXCL | 0644);
+  if(semd < 0)err();
+  union semun us;
+  us.val = 1;
+  int r = semctl(semd, 0, SETVAL, us);
+  if(r<0)err();
+
+  //file
+  int w_file = open("messages.txt", O_WRONLY | O_TRUNC | O_CREAT, 0600);
+  if(w_file < 0)err();
+  close(w_file);
+}
+
+void rem(){
+  union semun us;
+  int semd = semget(key, 1, 0 );
+  if(semd >= 0){
+    semctl(semd, 0, IPC_RMID, us);
+  }
+
+  int shmd = shmget(key, sizeof(int), 0);
+  if(shmd >= 0){
+    shmctl(shmd, IPC_RMID, 0);
+  }
+
+}
---
+++ shared.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <signal.h>
+
+
+#ifndef SHARED_H
+#define SHARED_H
+void create();
+void rem();
+#endif
