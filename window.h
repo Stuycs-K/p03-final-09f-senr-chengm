368b89c7e227d986a9fed6912c4f33577abbc97a
Rohan <83433649+RohanSen08@users.noreply.github.com>
2026-01-08 02:33:55 -0500

    Update window.h

---
+++ window.h
@@ -0,0 +1,18 @@
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+#ifndef WINDOW_H
+#define WINDOW_H
+#endif
