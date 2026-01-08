2874d123974d3ba2e3d0d5c7c1ab2b2a485cd7d8
Rohan <83433649+RohanSen08@users.noreply.github.com>
2026-01-08 02:33:38 -0500

    Update app.h

--- app.h
+++ app.h
@@ -1,1 +1,18 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+#ifndef APP_H
+#define APP_H
+#endif
