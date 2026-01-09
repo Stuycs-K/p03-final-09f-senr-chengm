78bd2e3cd8a6d09fd513199247d260e210a06a4d
Rohan <83433649+RohanSen08@users.noreply.github.com>
2026-01-09 00:52:24 -0500

    now compiling

--- app.c
+++ app.c
@@ -5,21 +5,24 @@ activate (GtkApplication *app,
           gpointer        user_data)
 {
   GtkWidget *window;
-  GtkWidget *button;
-  GtkEntryBuffer* buffer;
+  GtkWidget *box;
+  GtkWidget *message;
+  GtkEntryBuffer *buffer;
+  
   window = gtk_application_window_new (app);
-
+  box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 6);
+  gtk_window_set_child(GTK_WINDOW(window), box);
   GtkWidget *label = gtk_label_new("WIP: messages will appear here");
   gtk_label_set_xalign(GTK_LABEL(label), 0.0);
   gtk_box_append(GTK_BOX(box), label);
 
   gtk_window_set_title (GTK_WINDOW (window), "c_chat");
   gtk_window_set_default_size (GTK_WINDOW (window), 800, 200);
-  
+  buffer = gtk_entry_buffer_new(NULL, -1);
   message = gtk_entry_new_with_buffer(buffer);
   gtk_widget_set_halign(message, GTK_ALIGN_RIGHT);
   gtk_widget_set_valign(message, GTK_ALIGN_BOTTOM);
-  gtk_window_set_child (GTK_WINDOW (window), message);
+  gtk_box_append(GTK_BOX(box), message);
 
   gtk_window_present (GTK_WINDOW (window));
 }
