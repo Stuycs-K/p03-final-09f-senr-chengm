4301dc059d942d43dbd8eefa7d2a8b6bc25bca66
Matthew Cheng <cheng.matt.t1@gmail.com>
2026-01-09 00:09:49 -0500

    commented out code

--- client.c
+++ client.c
@@ -1,24 +1,24 @@
 #include "networkstructure.h"
 
 
-void clientLogic(int server_socket){
-  char buf[1024];
-  printf("Enter a message: \n");
-  if (fgets(buf, 1024, stdin) == NULL) {
-    close(server_socket);
-    printf("Client closed\n");
-    exit(0);
-  }
-  send(server_socket, buf, strlen(buf), 0);
-  int n = recv(server_socket, buf, 1024, 0);
-  if (n==0) {
-    close(server_socket);
-    printf("Client closed\n");
-    exit(0);
-  }
-  buf[n] = '\0';
-  printf("recieved: '%s'\n", buf);
-}
+// void clientLogic(int server_socket){
+//   char buf[1024];
+//   printf("Enter a message: \n");
+//   if (fgets(buf, 1024, stdin) == NULL) {
+//     close(server_socket);
+//     printf("Client closed\n");
+//     exit(0);
+//   }
+//   send(server_socket, buf, strlen(buf), 0);
+//   int n = recv(server_socket, buf, 1024, 0);
+//   if (n==0) {
+//     close(server_socket);
+//     printf("Client closed\n");
+//     exit(0);
+//   }
+//   buf[n] = '\0';
+//   printf("recieved: '%s'\n", buf);
+// }
 
 int main(int argc, char *argv[] ) {
   char* IP = "127.0.0.1";
